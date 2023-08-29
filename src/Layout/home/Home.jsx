636183786d760b4f9eda983003101f0cc158eb11
unknown <rajatgour333@gmail.com>
2023-08-29 11:30:27 +0530

    slider

--- src/Layout/home/Home.jsx
+++ src/Layout/home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import Layout from '../Layout'
 import AwsHostingCard from '../../components/awsHosting/AwsHostingCard'
+// import Slider from './Slider/SliderComponent'
+import SliderComponent from './Slider/SliderComponent'
 
 
 const Home = () => {
   return (
     <Layout>
     <AwsHostingCard/>
+    <SliderComponent/>
     </Layout>
   )
 }
